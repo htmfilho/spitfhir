b7206d458b83309ca2aae45a72e2ccf369078006
Hildeberto Mendonca <me@hildeberto.com>
2022-01-14 18:05:01 -0500

    Tiny main abstraction

--- cmd/spitfhir/main.go
+++ cmd/spitfhir/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	fmt.Println("SpitFHIR")
+	return nil
 }
